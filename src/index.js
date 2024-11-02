0162a460a1041054e677e3f0c0e93cd7d7795968
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-11-02 04:02:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
     ],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
